d134d17f29434cccdf997cfb8f5363e59994b4fa
Henrik Boulund <42587454+henrikboulund@users.noreply.github.com>
2023-09-08 15:14:51 +0200

    Added Person class.

---
+++ Classes and objects/Person.java
@@ -0,0 +1,25 @@
+public class Person()
+{
+    private String Name;
+    private int Age;
+    private String Occupation;
+
+    Person p = new Person("Hans",0,"Fitter");
+
+    public Person(String name, int age, String occupation)
+    {
+        Name = name;
+        Age = age;
+        Occupation = occupation;
+    }
+
+    public void Walk()
+    {
+        //**jfnbvdbnfc */
+    }
+
+    public void Sleep()
+    {
+
+    }
+}
